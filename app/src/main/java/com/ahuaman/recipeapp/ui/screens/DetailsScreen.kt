d35b745f92691e9a19ced10d23b25aeff6cb6d79
ahuamana <73962071@continental.edu.pe>
2023-07-21 02:29:37 -0500

    if applied I will implement the logo for the app

--- app/src/main/java/com/ahuaman/recipeapp/ui/screens/DetailsScreen.kt
+++ app/src/main/java/com/ahuaman/recipeapp/ui/screens/DetailsScreen.kt
@@ -26,7 +26,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
---
+++ app/src/main/java/com/ahuaman/recipeapp/ui/screens/Utils.kt
@@ -0,0 +1,16 @@
+package com.ahuaman.recipeapp.ui.screens
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+
+//wanna create a Brush rainbowColorsBrush
+val rainbowColorsBrush = Brush.linearGradient(
+    colors = listOf(
+        Color.Red,
+        Color.Yellow,
+        Color.Green,
+        Color.Blue,
+        Color.Magenta
+    )
+)
